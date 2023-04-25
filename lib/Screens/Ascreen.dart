f0c8ff0c4e3631d3a17dadef7fd679a16ec05ee3
MMY <omarmmy@gmail.com>
2023-04-25 03:02:47 +0200

    omar last

--- lib/Screens/Ascreen.dart
+++ lib/Screens/Ascreen.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+
+void main ()
+{
+  runApp(Ascreen());
+}
 class Ascreen extends StatelessWidget {
 
 
@@ -13,18 +18,105 @@ class Ascreen extends StatelessWidget {
             image: DecorationImage(
               image: AssetImage('assets/images/ooo.jfif',),
               fit: BoxFit.cover,
-            )
-        ),
+            )),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          body: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Center(
+              child: Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0,),
+                  color: Colors.black,
+                ),
+                child:
+                SingleChildScrollView(
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Are you Ok?',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 25
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      Text('Our sensors detected unusual speed and stop,'
+                          '  we just want to make sure everything is ok.',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 14
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20.0,
+                      ),
 
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            FloatingActionButton(
-                onPressed: () {},
-                child: Icon(Icons.phone_iphone_rounded))
-          ],
-        ),
+                      Row (children: [
+                        Padding(
+                          padding: const EdgeInsets.all(4.0),
+                          child:Container(
+                            width: 200,
+                            height: 40,
+                            child: ElevatedButton(onPressed: ()
+                            {
+                            },
+                              style:ElevatedButton.styleFrom(
+                                primary: Colors.red,
+                                                            ),
 
+                              child: Text('Call Emergency',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          width:100 ,
+                          height:40,
+
+                          child:ElevatedButton(onPressed: ()
+                            {
+                            },
+                            style:ElevatedButton.styleFrom(
+                                primary: Colors.green,
+
+                            ),
+                            child: Text('Yes',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                              ),
+                            ),
+                          ),
+
+                        ),
+                      ],
+
+                      ),
+
+                    ],
+
+
+                ),
+                  ),
+
+
+                ),
+
+
+              ),
+            ),
+          ),
+        ),
 
       ),
     );
